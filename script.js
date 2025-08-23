24caf0225f3564025ff41f587b5c1bd4def4234f
jlmcpeak <you@youremail.com>
2025-08-22 22:54:45 -0700

    stuff

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
 (function(){
     `use strict`;
     console.log(`running js`);
-
-    const typefaces = [`courier-new`, `franklin-gothic-medium`, `gill-sans`, `lucida-sans`, `segoe-ui`, `times-new-roman`, `trebuchet-ms`, `arial`, `cambria`, `georgia`, `impact`, `verdana`];
-
-    const fName = document.querySelector(`#f-name`);
-    const mI = document.querySelector(`#m-i`);
-    const lName = document.querySelector(`#l-name`);
-
-    setInterval(function(){
-        fName.setAttribute(`class`, `${typefaces[Math.floor(Math.random() * 12)]}`);
-    }, 200);
-
-    setInterval(function(){
-        mI.setAttribute(`class`, `${typefaces[Math.floor(Math.random() * 12)]}`);
-    }, 100);
-
-    setInterval(function(){
-        lName.setAttribute(`class`, `${typefaces[Math.floor(Math.random() * 12)]}`);
-    }, 300);
 })();
